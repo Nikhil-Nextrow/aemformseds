34db535d8220203e26e2984cd8e06bd55fb67eb4
Nikhil-Nextrow <nikhil.dasari@nextrow.com>
2026-04-23 17:47:43 +0530

    updating quote.js for testing an example

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,3 +1,14 @@
 export default async function decorate(block) {
     console.log(block);
+
+    fetch('/graphql/execute.json/wknd-shared/adventures-all')
+        .then(response => {
+            // if (!response.ok) throw new Error('Network error');
+            return response.json(); // Parses JSON response
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => console.error('Fetch Error:', error));
+
 }
